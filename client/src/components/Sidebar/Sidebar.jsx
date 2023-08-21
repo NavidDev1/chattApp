cfc7c47e493d6d7cf2949fdfbe9ab00266a6121a
RickardTimergalejevs <rickard.time@gmail.com>
2023-08-21 14:32:34 +0200

    Sidebar and chatfeed styling

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,8 +2,8 @@ import Room from "../Room/Room"
 
 function Sidebar() {
     return (
-      <div>
-        <p>Sidebar</p>
+      <div className="bg-slate-400 w-1/6">
+        <h1 className="text-4xl font-bold pb-8">Rooms:</h1>
         <Room />
       </div>
     )

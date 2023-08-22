bbd66c796f06d81762b9736cbe508b31ed5ed94d
Naviddev1 <Navid86@hotmail.se>
2023-08-22 09:22:16 +0200

    context and server changes

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,15 +1,21 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { io } from "socket.io-client"
+
+const socket = io("http://localhost:3000/", { autoConnect: false })
 
 const ChatContext = createContext()
 
-const socket = io("http://localhost:3000/", { autoConnect: false })
 export const useChatContext = () => useContext(ChatContext)
 
+
+
+
 const ChatProvider = ({ children }) => {
+  //setting the diffrent state.
     const [username, setUsername] = useState("")
     const [roomsList, setRoomsList] = useState([])
     const [currentRoom, setCurrentRoom] = useState("")
+    const [messages, setMessages] = useState([]);
 
     const connectToChat = () => {
         if (username) {
@@ -19,8 +25,35 @@ const ChatProvider = ({ children }) => {
         }
     }
 
+    useEffect(() => {
+      // Listen for "message" events from the socket and update the messages state
+      socket.on("message", (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      });
+    }, []);
+  
+    // Function to send a message using the socket, here we send an object so that we can retirve username.
+    const sendMessage = (message) => {
+      if (message.trim() !== "") {
+        const messageData ={
+          username: username,
+          content: message,
+        };
+        socket.emit("message", messageData);
+      }
+    };
+  
+    // The value to provide through the context
+    const chatContextValue = {
+      messages,
+      sendMessage,
+      username,
+      setUsername,
+      connectToChat
+    };
 
-    return (<ChatContext.Provider value={{ username, setUsername, connectToChat }}>
+
+    return (<ChatContext.Provider value={chatContextValue}>
         {children}
     </ChatContext.Provider>)
 }
--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
-const express = require("express")
-const http = require("http")
-const { Server } = require("socket.io")
-const cors = require("cors")
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-app.use(cors())
+app.use(cors());
 
 io.on("connection", (socket) => {
-    console.log("New client connected:", socket.id)
-})
+  console.log("New client connected:", socket.id);
 
-server.listen(3000, () => console.log("Server is up and running"))
+  // Listen for messages from clients
+  socket.on("message", (messageData) => {
+    console.log("Received message:", messageData);
+
+    // Broadcast the message to all connected clients, including the sender
+    io.emit("message", messageData);
+  });
+
+  // Handle client disconnect
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => console.log("Server is up and running"));

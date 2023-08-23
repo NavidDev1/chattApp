ae81ed29d52e7abe811394db598b4155fe8742fd
RickardTimergalejevs <rickard.time@gmail.com>
2023-08-23 14:08:51 +0200

    Redirect to "/" if username is empty

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,22 @@
 import './App.css'
-import {BrowserRouter, Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import StartPage from "./pages/StartPage"
 import Main from './components/Main/Main'
+import { useChatContext } from './context/ChatContext'
 
 function App() {
+  const {username} = useChatContext()
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<StartPage />} />
-          <Route path="/lobby" element={<Main />} />
-          {/* <Route path="/lobby/:id" element={<Lobby />} />
-          <Route path="/create_room" element={<CreateRoom />} />
-          <Route path="/all_rooms" element={<AllRooms />} /> */}
+          <Route path="/lobby" element={username ? <Main /> : <Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
   )
 }
-
-{/* <Main />
-<MessageInput />
-<StartPage /> */}
 
 export default App
--- client/src/components/CreateRoomPopup/CreateRoomPopup.jsx
+++ client/src/components/CreateRoomPopup/CreateRoomPopup.jsx
@@ -17,7 +17,7 @@ function CreateRoomPopup({ onClose }) {
     return (
       <div className="bg-slate-500 flex flex-col items-center justify-center gap-2 absolute w-96 h-60 top-0 right-0 m-40">
         <h1>Create Room</h1>
-        <input type="text" value={newRoom} onChange={(e) => setNewRoom(e.target.value)} />
+        <input type="text" value={newRoom} onChange={(e) => setNewRoom(e.target.value)} className="text-black"/>
         <button onClick={handleCreateRoom}>Create</button>
         <button onClick={onClose}>Close</button>
       </div>

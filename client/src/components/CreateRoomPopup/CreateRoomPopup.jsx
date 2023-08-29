e07b342a734c230ae1f6f1798fe84b03f9a5d41f
RickardTimergalejevs <rickard.time@gmail.com>
2023-08-29 11:53:00 +0200

    Create room popup style

--- client/src/components/CreateRoomPopup/CreateRoomPopup.jsx
+++ client/src/components/CreateRoomPopup/CreateRoomPopup.jsx
@@ -14,14 +14,33 @@ function CreateRoomPopup({ onClose }) {
   }
 
   console.log(newRoom);
-    return (
-      <div className="bg-slate-500 flex flex-col items-center justify-center gap-2 absolute w-96 h-60 top-0 right-0 m-40">
-        <h1>Create Room</h1>
-        <input type="text" value={newRoom} onChange={(e) => setNewRoom(e.target.value)} className="text-black"/>
-        <button onClick={handleCreateRoom}>Create</button>
-        <button onClick={onClose}>Close</button>
+  return (
+    <div className="flex flex-col items-center justify-center fixed inset-0 z-50">
+      <div className="bg-gray-500 rounded-lg p-4 ml-36">
+        <h1 className="text-center mb-2">Create Room</h1>
+        <input
+          type="text"
+          value={newRoom}
+          onChange={(e) => setNewRoom(e.target.value)}
+          className="text-black p-1 border border-gray-300 mb-2 w-60"
+        />
+        <div className="flex justify-center gap-2">
+        <button
+          onClick={handleCreateRoom}
+          className="bg-green-500 text-sm text-white px-4 py-1"
+        >
+          Create
+        </button>
+        <button
+          onClick={onClose}
+          className="bg-gray-600 text-sm text-white px-4 py-1"
+        >
+          Close
+        </button>
+        </div>
       </div>
-    )
+    </div>
+  )
   }
   
   export default CreateRoomPopup
